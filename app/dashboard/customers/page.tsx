fa52fa1e588acdae2b40efe5974146e803c57386
Tim Marcus Moore <tmoore@incrementalism.net>
2024-05-05 14:41:40 +0930

    Add customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,24 @@
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers: FormattedCustomersTable[] =
+    await fetchFilteredCustomers(query);
+
+  return <Table customers={customers} />;
 }
